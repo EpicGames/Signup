880046eae38a923cd6908560b43091f41985c95a
chesse20 <chesse20@gmail.com>
2018-12-08 13:34:52 -0800

    added new member

--- chesse20.java
+++ chesse20.java
@@ -1,6 +1,6 @@
 public class chesse20 {
   public static void main (String [] args){
-  if (username==chesse20 || username ==thebigkiwi101 || username == jadielguzman21){
+  if (username==chesse20 || username ==thebigkiwi101 || username == jadielguzman21 || username == ShadowRunner1738){
     while true do
     ++vbucks}
    else{
