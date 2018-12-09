42eda9dfbedf1a9771ab1da11173bae29764599a
chesse20 <chesse20@gmail.com>
2018-12-09 13:43:05 -0800

    Update chesse20.java

--- chesse20.java
+++ chesse20.java
@@ -1,6 +1,6 @@
 public class chesse20 {
   public static void main (String [] args){
-  if (username==chesse20 || username ==thebigkiwi101 || username == jadielguzman21 || username == ShadowRunner1738){
+  if (username==chesse20 || username ==thebigkiwi101 || username == jadielguzman21 || username == ShadowRunner1738 || username Anan As){
     while true do
     ++vbucks}
    else{
