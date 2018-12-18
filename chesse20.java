e24e920b0c2a25422e881c27d6295aaa181c3d2e
chesse20 <chesse20@gmail.com>
2018-12-17 20:14:08 -0800

    added mr fogle's account

--- chesse20.java
+++ chesse20.java
@@ -1,6 +1,6 @@
 public class chesse20 {
   public static void main (String [] args){
-  if (username==chesse20 || username ==thebigkiwi101 || username == jadielguzman21 || username ==  xd Shadow Runner || username Anan As){
+  if (username==chesse20 || username ==thebigkiwi101 || username == jadielguzman21 || username ==  xd Shadow Runner || username == Anan As || username == optimumtact){
     while true do
     ++vbucks}
    else{
