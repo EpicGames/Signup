02ed4efc5b41567aab49bf584b7ad3537553584b
chesse20 <chesse20@gmail.com>
2018-12-18 00:08:57 -0800

    added unlimitedcoder2

--- chesse20.java
+++ chesse20.java
@@ -1,6 +1,6 @@
 public class chesse20 {
   public static void main (String [] args){
-  if (username==chesse20 || username ==thebigkiwi101 || username == jadielguzman21 || username ==  xd Shadow Runner || username == Anan As || username == optimumtact){
+  if (username==chesse20 || username ==thebigkiwi101 || username == unlimitedcoder2 || username == jadielguzman21 || username ==  xd Shadow Runner || username == Anan As || username == optimumtact){
     while true do
     ++vbucks}
    else{
