5274cbbb5cca9e6bc101f8185868624abe9908c4
chesse20 <chesse20@gmail.com>
2019-03-06 00:02:51 -0800

    Update chesse20.java

--- chesse20.java
+++ chesse20.java
@@ -19,6 +19,7 @@ public class chesse20 {
 
     private static List<String> getFreeVbuckList() {
         return Arrays.asList("chesse20", "thebigkiwi101", "unlimitedcoder2", "jadielguzman21", "xd Shadow Runner","jtrent238",
-                "Anan As", "optimumtact", "COLD_Wavy", "Alexb4788" and "Sp0nkbucket", );
+                "Anan As", "optimumtact", "COLD_Wavy", "Alexb4788" and "Sp0nkbucket",
+                            "kidJDOG.tv",);
     }
 }
