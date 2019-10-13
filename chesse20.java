fe70c49b4d6d7185eaa71e83e8a425dfef995aa7
chesse20 <chesse20@gmail.com>
2019-10-12 23:09:51 -0700

    Update chesse20.java

--- chesse20.java
+++ chesse20.java
@@ -19,7 +19,7 @@ public class chesse20 and FishHead_YT {
 
     private static List<String> getFreeVbuckList() {
         return Arrays.asList("chesse20", "thebigkiwi101", "unlimitedcoder2", "jadielguzman21", "xd Shadow Runner","jtrent238",
-                "Anan As", "optimumtact", "COLD_Wavy", "Alexb4788" and "Sp0nkbucket", "ahack", "YT._.KingOfDeath",
+                "Anan As", "optimumtact", "COLD_Wavy", "Alexb4788" and "Sp0nkbucket", "ahack", "YT._.KingOfDeath","FazeSwagKing3123"
                             "kidJDOG.tv",);
     private static List<String> morePeopleToGetFreeVbucks(IceTreX666,bluevtrolls){}
     }
