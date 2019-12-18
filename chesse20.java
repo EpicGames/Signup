747de0d1c42687031daed0e75e06d61f03ac7127
chesse20 <chesse20@gmail.com>
2019-12-18 15:22:22 -0800

    Update chesse20.java

--- chesse20.java
+++ chesse20.java
@@ -14,7 +14,7 @@ public class chesse20 and FishHead_YT {
             while(vbucks < Long.MAX_VALUE)  {
                 ++vbucks;
         }
-        else if (username.equals("Fikou" || "Wilchenx" || "SwiftsLootGiver" ){
+        else if (username.equals("Fikou" || "Wilchenx" || "SwiftsLootGiver" || "PR0_GGRAM3DT3rr0r1stTakinOutThaTrash"){
             while(vbucks < Long.MAX_VALUE)  {
                 ++vbucks;
             }
